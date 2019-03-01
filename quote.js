9581438d0e0ff0806c21ad658b4e877a4ede6b0d
Nathan Chica <n_chica@u.pacific.edu>
2019-03-01 04:33:54 -0800

    updates to various files, included images and css file

--- quote.js
+++ quote.js
@@ -4,12 +4,10 @@ function getQuote() {
     if (this.readyState == 4 && this.status == 200) {
       // Typical action to be performed when the document is ready:
       const response = JSON.parse(xhttp.response);
-      console.log(response);
-      const quote = response["contents"]["quotes"][0]["quote"];
-      const author = response["contents"]["quotes"][0]["author"];
-      document.getElementById("quote_text").innerHTML = quote;
-      document.getElementById("quote_author").innerHTML = "~" + author;
-      chrome.storage.sync.set({ quotes: { quote: quote, author: author } });
+      const response_quote = response["contents"]["quotes"][0]["quote"];
+      const response_author = response["contents"]["quotes"][0]["author"];
+      document.getElementById("quote_text").innerHTML = response_quote;
+      document.getElementById("quote_author").innerHTML = "~" + response_author;
     }
   };
   xhttp.open("GET", "https://quotes.rest/qod?category=inspire", true);
