868f1abe66c70e5792c3f7f057ba1d4856339bb5
Nathan Chica <n_chica@u.pacific.edu>
2019-04-17 12:13:33 -0700

    updates to options and settings

--- settings.js
+++ settings.js
@@ -7,14 +7,7 @@ M.Dropdown.init(settingsIcon, {
 
 const settingsDropdownInstance = M.Dropdown.getInstance(settingsIcon);
 
-document.getElementById("settings-form").onsubmit = event => {
-  if (event) {
-    event.preventDefault();
-  }
-  settingsDropdownInstance.close();
-};
-
-document.getElementById("settings-save-btn").onclick = () => {
+const saveAndUpdateValues = () => {
   const nameValue = document.getElementById("name_input").value;
   const locationValue = document.getElementById("location_input").value;
 
@@ -24,4 +17,20 @@ document.getElementById("settings-save-btn").onclick = () => {
     },
     updateName
   );
+};
+
+const initSettings = () => {
+  chrome.storage.sync.get("settings", result => {
+    document.getElementById("name_input").value = result.settings.name;
+    document.getElementById("location_input").value = result.settings.location;
+    M.updateTextFields();
+  });
+};
+
+document.getElementById("settings-form").onsubmit = event => {
+  if (event) {
+    event.preventDefault();
+  }
+  settingsDropdownInstance.close();
+  saveAndUpdateValues();
 };
